70842b191147e71265917ffa8a88ea6280d06b0c
Islam Dudaev <islamwolf23@gmail.com>
2019-08-07 21:48:00 +0100

    Create Single.js component

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Photo from './Photo'
+
+class PhotoGrid extends Component {
+  render () {
+    return (
+      <div className='photo-grid'>
+        {this.props.posts.map((post, i) => {
+          return <Photo {...this.props} key={i} index={i} post={post} />
+        })}
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid
