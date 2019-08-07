c925579493301b652b22c1af015b5f9ca4019bc8
Islam Dudaev <islamwolf23@gmail.com>
2019-08-07 15:06:40 +0100

    Add Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/style.css'
-import App from './components/App'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/style.css";
+import Main from "./components/Main";
+import PhotoGrid from "./components/PhotoGrid";
+import Single from "./components/Single";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const router = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Main}>
+        <Route component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+)
+ReactDOM.render(router, document.getElementById("root"));
